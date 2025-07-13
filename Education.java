b1384ef70e2d548dfa0600edf8e0eb8934a234bd
sriniharika20 <155428177+sriniharika20@users.noreply.github.com>
2025-07-13 18:19:30 +0530

    Create Education.java

---
+++ Education.java
@@ -0,0 +1,13 @@
+Class Education{
+public static void main(String[] args){
+  System.out.print("college:");
+  System.out.println(args[0]);
+  System.out.print("Name:");
+  System.out.println(args[1]);
+  System.out.print("Branch:");
+  System.out.println(args[2]);
+  System.out.print("Graduation year:");
+  System.out.print(args[3]);
+  }
+}
+  
