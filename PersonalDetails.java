5d7fd647e63472a7f4ca546a66bbdf5293a8f914
sriniharika20 <155428177+sriniharika20@users.noreply.github.com>
2025-07-13 18:54:28 +0530

    PersonalDetails.java

--- PersonalDetails.java
+++ PersonalDetails.java
@@ -1,9 +1,9 @@
 class PersonalDetails{
   public static void main(String[] args){
     System.out.print("Name:");
-    System.out.print(args[0]);
+    System.out.println(args[0]);
     System.out.print("Gender:");
-    System.out.print(args[1]);
+    System.out.println(args[1]);
     System.out.print("Age:");
     System.out.print(args[2]);
   }
