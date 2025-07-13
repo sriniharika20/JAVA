61a5d586e06bc251308b4a1c7e534eecb72cc724
sriniharika20 <155428177+sriniharika20@users.noreply.github.com>
2025-07-13 18:23:44 +0530

    Create Family.java

---
+++ Family.java
@@ -0,0 +1,14 @@
+class Family{
+Public static void main(String[]args){
+  System.out.print("Father:");
+  System.out.println(args[0]);
+  System.out.print("Mother:");
+  System.out.println(args[1]);
+  System.out.print("Brother:");
+  System.out.println(args[2]);
+  System.out.print("Sister:");
+  System.out.print(args[3]);
+ }
+}
+  
+  
