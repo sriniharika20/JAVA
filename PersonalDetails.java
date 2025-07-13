a3b4c1b8efd59aa3ea220bc6b5c8ed0f441186f3
sriniharika20 <155428177+sriniharika20@users.noreply.github.com>
2025-07-13 18:27:38 +0530

    Create PersonalDetails.java

---
+++ PersonalDetails.java
@@ -0,0 +1,10 @@
+class PersonalDetails{
+  public static void main(String[] args){
+    System.out.print("Name:");
+    System.out.print(args[0]);
+    System.out.print("Gender:");
+    System.out.print(args[1]);
+    System.out.print("Age:");
+    System.out.print(args[2]);
+  }
+}
