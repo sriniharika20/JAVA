678dbec4ea20a8aeb59396f72a90a62b5f2c741b
sriniharika20 <155428177+sriniharika20@users.noreply.github.com>
2025-07-13 23:19:34 +0530

    Family.java

--- Family.java
+++ Family.java
@@ -1,12 +1,12 @@
 class Family{
 Public static void main(String[]args){
-  System.out.print("Father:");
+  System.out.print("Father name:");
   System.out.println(args[0]);
-  System.out.print("Mother:");
+  System.out.print("Mother name:");
   System.out.println(args[1]);
-  System.out.print("Brother:");
+  System.out.print("Brother name:");
   System.out.println(args[2]);
-  System.out.print("Sister:");
+  System.out.print("Sister name:");
   System.out.print(args[3]);
  }
 }
